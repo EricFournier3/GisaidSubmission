84da126b85e8f0b2a40204f9782b63b5df3aa633
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-05-19 16:38:40 -0400

    ef

--- CovGisaidSubmission.sh
+++ CovGisaidSubmission.sh
@@ -5,9 +5,17 @@ echo "TESTING"
 unpublished_path="/data/Runs/SARS-CoV-2/GenomeCenterSeq/FinalRelease/TEST/FinalUnpublished/"
 submitted_path="/data/Runs/SARS-CoV-2/GenomeCenterSeq/FinalRelease/TEST/FinalSubmitted/"
 
+slbio_user=$(whoami)
+ldap_user=$(echo ${slbio_user} | cut -d '@' -f 1)
+PASS_FILE="/home/${slbio_user}/pass.txt"
 
-<<COM
 nb_mounted_rep=$(ls /mnt/Partage/ | wc -l)
+
+green_message="\e[32m"
+white_message="\e[39m"
+red_message="\e[31m"
+yellow_message="\e[33m"
+
 
 if [ $nb_mounted_rep -gt 0 ]
         then
@@ -20,9 +28,6 @@ else
         read pw < $PASS_FILE
         sudo mount -t cifs -o username=${ldap_user},password=$pw,vers=3.0 "//swsfi52p/partage" /mnt/Partage
 fi
-COM
-
-
 
 
 #python /data/Applications/GitScript/GisaidSubmission/CovGisaidSubmission.py --unpublished-path  ${unpublished_path} --submitted-path ${submitted_path}
